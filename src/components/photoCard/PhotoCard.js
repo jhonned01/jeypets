d2b060faf462ee09295ffad0ae21066b9a13f0c7
john delgado <jhonned01@gmail.com>
2020-12-28 15:37:03 -0500

    intersection obsrever for navegation

--- src/components/photoCard/PhotoCard.js
+++ src/components/photoCard/PhotoCard.js
@@ -10,19 +10,23 @@ export default function PhotoCard({ id, likes = 0, src }) {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    import("intersection-observer");
+    Promise.resolve(
+      typeof window.IntersectionObserver !== "undefined"
+        ? window.IntersectionObserver
+        : import("intersection-observer")
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0];
 
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0];
-      console.log({ isIntersecting });
-      if (isIntersecting) {
-        console.log("si");
-        setShow(true);
-        observer.disconnect();
-      }
+        if (isIntersecting) {
+          console.log("si");
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+
+      observer.observe(element.current);
     });
-
-    observer.observe(element.current);
   }, [element]);
 
   return (
