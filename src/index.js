84f530a69ed506d1701d17363924a1a0b206dfdd
jhonned01 <jhonned01@gmail.com>
2021-02-09 23:53:20 -0500

    cambiando el headers a sesion storage

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
   // return the headers to the context so httpLink can read them
+
+  console.log("token headers:");
+  console.log(token);
+  console.log("====================================");
   return {
     headers: {
       ...headers,
